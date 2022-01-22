e5c9678d875a804c8685ee0eab8a18b8ec9e5330
Z <zwang2018@gmail.com>
2022-01-22 15:29:30 -0500

    -- added in UI for create new announcement and new event

---
+++ app/src/main/java/com/example/first_responder_app/NewAnnouncementViewModel.java
@@ -0,0 +1,7 @@
+package com.example.first_responder_app;
+
+import androidx.lifecycle.ViewModel;
+
+public class NewAnnouncementViewModel extends BaseViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/first_responder_app/NewEventFragment.java
@@ -0,0 +1,36 @@
+package com.example.first_responder_app;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewEventFragment extends Fragment {
+
+    private NewEventViewModel mViewModel;
+
+    public static NewEventFragment newInstance() {
+        return new NewEventFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event_new, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(NewEventViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
