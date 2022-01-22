ed7d238a06ccc9c61d4642c754a572f173050471
Z <zwang2018@gmail.com>
2022-01-21 19:04:45 -0500

    -- initial push: setup login page GUI as well as initial nav_map -- added Announcement GUI & Event Group GUI -- added Event GUI

--- app/src/main/java/com/example/first_responder_app/EventFragment.java
+++ app/src/main/java/com/example/first_responder_app/EventFragment.java
@@ -12,24 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EventGroupFragmen extends Fragment {
+public class EventFragment extends Fragment {
 
-    private EventGroupViewModel mViewModel;
+    private EventViewModel mViewModel;
 
-    public static EventGroupFragmen newInstance() {
-        return new EventGroupFragmen();
+    public static EventFragment newInstance() {
+        return new EventFragment();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_event_group, container, false);
+        return inflater.inflate(R.layout.fragment_event, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(EventGroupViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(EventViewModel.class);
         // TODO: Use the ViewModel
     }
 
---
+++ app/src/main/java/com/example/first_responder_app/EventViewModel.java
@@ -0,0 +1,7 @@
+package com.example.first_responder_app;
+
+import androidx.lifecycle.ViewModel;
+
+public class EventViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
