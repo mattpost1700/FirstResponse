2e630efdd2f4a41ed12e87eeb0f918d7a445dc20
Z <zwang2018@gmail.com>
2022-01-28 19:56:16 -0500

    -- put everything in folders so they don't look like a pile of garbo

---
+++ app/src/main/java/com/example/first_responder_app/viewModels/IncidentViewModel.java
@@ -0,0 +1,7 @@
+package com.example.first_responder_app.viewModels;
+
+import androidx.lifecycle.ViewModel;
+
+public class IncidentViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
